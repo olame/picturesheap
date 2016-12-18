32079bfa68a5feccc8f758dbc2a8d746fc13c551
Olga Grineva <olame@ya.ru>
2016-12-18 12:14:18 +0300

    fix bug about pictire click propagatiom
    fix bug about pictire click propagatiom

--- testApi.jsx
+++ testApi.jsx
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import NotificationSystem from 'react-notification-system';
 import $ from 'jquery';
 import {
     Map,
@@ -24,6 +25,14 @@ const flickrApiKey = 'd85870ac341987e679c7bff28adc0c30';
 //     const initPoint = {lat: 59.918606, lng: 30.348530};
 //     resolve(getPlaceUrl(initPoint.lat, initPoint.lng));
 // });
+
+const setState = React.Component.prototype.setState;
+React.Component.prototype.setState = function (...args) {
+    try {
+        console.log('setState', JSON.stringify(args[0]));
+    } catch (e) {}
+    return setState.apply(this, args);
+};
 
 class flickrApi {
     constructor(){
@@ -65,11 +74,11 @@ class flickrApi {
         }});
     }
 
-    photossearch ({lat, lon}) {
+    photossearch ({lat, lon, lng}) {
         return this._callApi({query: {
             method: 'flickr.photos.search',
             lat: lat,
-            lon: lon,
+            lon: lon || lng,
             radius: 20,
             extras: 'url_sq, geo',
         }});
@@ -95,7 +104,7 @@ class PhotosLayer extends MapControl {
 
         gallery.onAdd = () => {
             const div = L.DomUtil.create('div', 'photosGallery');
-            $(div).on('mousewheel', function(e){
+            $(div).on('mousewheel', function mousewheelHandler (e) {
                 //to stop catch event of mousewheel
                 e.stopPropagation();
             });
@@ -111,7 +120,7 @@ class PhotosLayer extends MapControl {
 
     _render () {
         ReactDOM.render(
-            <Photos size={this.props.size} onPhotoSelected={this.props.onPhotoSelected.bind(this)}/>, this.div
+            <Photos size={this.props.size} place={this.props.place} onPhotoSelected={this.props.onPhotoSelected.bind(this)}/>, this.div
         );
     }
 
@@ -128,28 +137,40 @@ class Photos extends React.Component {
             place: {lat: 59.918606, lon: 30.348530},
             photos: [],
         };
-        const photos = flickr.photossearch(this.state.place);
-        photos.then(data => {
+        this._previousPlace = null;
+    }
+
+    componentDidUpdate(){
+        const place = this.props.place;
+        if (_.isEqual(place, this._previousPlace)){
+            return;
+        }
+        this._previousPlace = place;
+
+        flickr.photossearch(place).then(data => {
             return data.photos.photo.map(i => ({
                 id: i.id,
                 lat: i.latitude,
                 lon: i.longitude,
                 url: i.url_sq,
             }));
-        }).then(photos => this.setState({photos}));
+        }).then(photos => {
+            this.setState({photos});
+        });
     }
 
     render(){
         return (<div className="photos" style={{overflow: 'auto', width: this.props.size.x}}>
            <div style={{whiteSpace: 'nowrap'}}>
                {this.state.photos.map(photo =>
-                   <img onClick={this.handleClick.bind(this, photo)} key={photo.id} src={photo.url}/>
+                   <PictureItem onClick={this.handleClick.bind(this, photo)} key={photo.id} src={photo.url}/>
                )}
            </div>
        </div>);
     }
 
-    handleClick(photo){
+    handleClick(photo, e){
+        e.stopPropagation();
         if(this.props.onPhotoSelected && photo){
             this.props.onPhotoSelected(photo);
         }
@@ -159,6 +180,41 @@ class Photos extends React.Component {
 Photos.propTypes = {
     size: React.PropTypes.object,
     onPhotoSelected: React.PropTypes.func,
+    place: React.PropTypes.object,
+};
+
+class PictureItem extends React.Component {
+    constructor (...args) {
+        super(...args);
+
+        this._boundElements = new WeakSet;
+    }
+
+    render () {
+        return <img ref={this._imgRef.bind(this)} src={this.props.src}/>;
+    }
+
+    _imgRef (img) {
+        this._bindImgEvents(img);
+    }
+
+    // NOTE, WORKAROUND: Bind native events directly on element instead of react events
+    // since react event are processed later then native (see react event batching)
+    _bindImgEvents (img) {
+        if (!img) {
+            return;
+        }
+        if (this._boundElements.has(img)) {
+            return;
+        }
+        $(img).on('click', this.props.onClick);
+        this._boundElements.add(img);
+    }
+}
+
+PictureItem.propTypes = {
+    onClick: React.PropTypes.func,
+    src: React.PropTypes.string,
 };
 
 class Main extends React.Component {
@@ -167,21 +223,25 @@ class Main extends React.Component {
         this.state = {
             marker: null,
             place: {lat: 59.918606, lon: 30.348530},
-            position: [59.918606, 30.348530],
+            mapPosition: [59.918606, 30.348530],
             size: {},
         };
 
     }
 
+    componentDidMount() {
+        this._notificationSystem = this.refs.notificationSystem;
+    }
 
     render() {
         const zoom = 13;
 
         return (
             <div id="world">
-                <FixedMap center={this.state.position} zoom={zoom} zoomControl={false}
+                <FixedMap center={this.state.mapPosition} zoom={zoom} zoomControl={false}
                     onResize={this.handleResize.bind(this)}
-                    onLoad={this.handleLoad.bind(this)}>
+                    onLoad={this.handleLoad.bind(this)}
+                    onClick={this.handleClick.bind(this)}>
                     <PhotosLayer place={this.state.place} size={this.state.size} onPhotoSelected={this.handlePhotoSelected.bind(this)} />
                     <ZoomControl />
                         <TileLayer
@@ -191,15 +251,29 @@ class Main extends React.Component {
                             <Marker position={this.state.marker} />
                             : null
                         }
-                    </FixedMap>
+                </FixedMap>
+                <NotificationSystem ref="notificationSystem" />
+
             </div>
         );
     }
+
+    _addNotification(message) {
+        this._notificationSystem.addNotification({
+            message: message,
+            level: 'success',
+            autoDismiss: 1,
+            position: 'bc',
+        });
+    }
+
+    handleClick({latlng: coordinate}){ //из первого элта получить ключ таргет и присвоить в leafletElement
+        this._addNotification('new coordinate selected');
+        this.setState({place: coordinate});
+    }
     handlePhotoSelected(photo){
         const markerPos = {lat: Number(photo.lat), lng: Number(photo.lon)};
-        this.setState({marker : markerPos});
-        this.setState({position: markerPos});
-        console.log('photo selected! '+ photo.lat + ' ' + photo.lon);
+        this.setState({marker : markerPos, mapPosition: markerPos});
     }
 
     handleResize({newSize: newSize}){
